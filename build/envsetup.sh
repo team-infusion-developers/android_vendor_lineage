311b37dfcd3442063680fa3621f470e79c799b56
Shakthivel Nadar <45127011+Sonic-sakthivel123@users.noreply.github.com>
2019-09-08 14:38:00 +0530

    Typo

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -103,7 +103,7 @@ alias bib=breakfast
 function eat()
 {
     if [ "$OUT" ] ; then
-        ZIPPATH=`ls -tr "$OUT"/Optimized-LineageOS-16.0*.zip | tail -1`
+        ZIPPATH=`ls -tr "$OUT"/Optimized-LineageOS-*.zip | tail -1`
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
             return 1
